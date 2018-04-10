172fb8c3b5cfa4f75331c22e5eed255f37ebe813
akashmore <akashmore1997.am85@gmail.com>
2018-04-10 18:59:24 +0530

    all modified changes

--- watchdocs.py
+++ watchdocs.py
@@ -1,41 +1,44 @@
-
 from flask import *
-from flask import Flask,render_template
-import os,sys,json
+from flask import Flask, render_template
+import os, sys, json, nltk, gensim, pickle
+from gensim import corpora, models, similarities
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from pymongo import MongoClient
 from flask import send_from_directory
 import textrazor
-#database connection
+
+# database connection
 client = MongoClient('localhost', 27017)
 mydb = client['watchdoc']
-collection=mydb['doccat']
-#UPLOAD_FOLDER ='C://Users//champ//Desktop//watchdoc1.0//upload'
-UPLOAD_FOLDER='C://Users//champ//Documents//watchdocgit//upload'
+collection = mydb['doccat']
+UPLOAD_FOLDER = 'C://Users//champ//Downloads//watchdocs-master(5)//upload'
+# UPLOAD_FOLDER='D:\\Study\\Final Year Sem 1\\Project\\watchdocs-master\\upload'
 
-#start app
+# start app
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-#index
+
+# index
 @app.route('/')
 @app.route('/index')
 def index():
+    return render_template('index.html')
 
-        return render_template('index.html')
 
-
-#upload file
+# upload file
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         filename = secure_filename(file.filename)
-        print(filename)
+       # print(filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return redirect(url_for('categarize',filename=filename))
-#categorize file
+    return redirect(url_for('categarize', filename=filename))
+
+
+# categorize file
 @app.route('/categarize/<filename>')
 def categarize(filename):
     finalcat = []
@@ -46,8 +49,8 @@ def categarize(filename):
     path = app.config['UPLOAD_FOLDER'] + '//' + filename
     client.set_classifiers(["textrazor_newscodes"])
     input_file = file(path).read().decode("utf-8")
-    startLines= input_file[0:100]
-    print(startLines)
+    startLines = input_file[0:100]
+    #print(startLines)
     response = client.analyze(input_file)
     entities = list(response.entities())
     entities.sort(key=lambda x: x.relevance_score, reverse=True)
@@ -56,7 +59,7 @@ def categarize(filename):
     info = list()
     for entity in entities:
         if entity.id not in seen:
-            #print (entity.id, entity.relevance_score, entity.confidence_score, entity.freebase_types)
+            # print (entity.id, entity.relevance_score, entity.confidence_score, entity.freebase_types)
             seen.add(entity.id)
             keywords.append(entity.id)
     mydb.keywords.insert({"keywords": keywords, "name": filename})
@@ -65,7 +68,7 @@ def categarize(filename):
     for topic in response.topics():
 
         if topic.score > 0.3:
-            #print (topic.label)
+            # print (topic.label)
             topiclist.append(topic.label)
             mydb.topic.insert({"topic": topic.label})
 
@@ -80,49 +83,68 @@ def categarize(filename):
             k = finalcat[0]
             s = finalscore[0]
 
-            #print(category.label)
-            print(alterLabel[-1])
-            #print category.score
+            # print(category.label)
+            # print(alterLabel[-1])
+            # print category.score
             categorylist.append(alterLabel[-1])
             mydb.category.insert({"category": alterLabel[-1]})
-        mydb.doccat.insert({"classified": k, "Document": filename, "Score":s,"startLines":startLines })
-        mydb.record.insert({"name": filename, "description": [{"keywords": keywords, "topic": topiclist, "category": categorylist}]})
+        mydb.doccat.insert({"classified": k, "Document": filename, "Score": s, "startLines": startLines})
+        mydb.record.insert(
+            {"name": filename, "description": [{"keywords": keywords, "topic": topiclist, "category": categorylist}]})
         output = "Category : " + str(k)
         return jsonify(result=output)
     except:
         return jsonify(result="unable to categarize")
-@app.route('/search',methods = ['POST', 'GET'])
+
+
+@app.route('/search', methods=['POST', 'GET'])
 def search():
-    jsonResultDocuments={}
-    docScore={}
-    resultdocuments={}
+    jsonResultDocuments = {}
+    docScore = {}
+    resultdocuments = {}
     if request.method == 'POST':
         keyword = request.form['searchkeyword']
+        keyword = keyword.lower()
+        model = gensim.models.Word2Vec.load("modelfile")
+        resultlist = model.most_similar(keyword)
+        #print(resultlist)
+        numberofresults = 7
+        # print "Words similar to searched keyword are"
+        for i in range(numberofresults):
+            print resultlist[i][0]
+        for document in collection.find():
+            category = document['classified'].lower()
+            if (keyword in category or category in keyword):
+                 docScore[document['Document']] = document['Score'], document['startLines']
+                 # print(docScore)
+            for i in range(len(resultlist)):
+                if (resultlist[i][0] in category or category in resultlist[i][0]):
+                    docScore[document['Document']] = document['Score'], document['startLines']
+                    # print(docScore)
+                for key, value in sorted(docScore.iteritems(), key=lambda (k, v): (v, k), reverse=True):
+                    resultdocuments[key] = value[1]
+    jsonResultDocuments["files"] = resultdocuments
+    return redirect(url_for('searchResult', filejson=json.dumps(jsonResultDocuments)))
 
-        for document in collection.find({"classified":keyword}):
-            docScore[document['Document']]=document['Score'],document['startLines']
-            print(docScore)
-        for key, value in sorted(docScore.iteritems(), key=lambda (k, v): (v, k),reverse=True):
-            resultdocuments[key]=value[1]
-        jsonResultDocuments["files"]=resultdocuments
-    return redirect(url_for('searchResult',filejson=json.dumps(jsonResultDocuments)))
 
 @app.route('/searchResult/<filejson>')
 def searchResult(filejson):
-     print(filejson)
-     responejson=json.loads(filejson)
-     fileArray=responejson["files"]
-     #print(fileArray)
-     return render_template('search.html',files=fileArray)
+    # print(filejson)
+    responejson = json.loads(filejson)
+    fileArray = responejson["files"]
+    # print(fileArray)
+    return render_template('search.html', files=fileArray)
 
 
-#for reading files
-@app.route('/fileread',methods=['GET','POST'])
+# for reading files
+@app.route('/fileread', methods=['GET', 'POST'])
 def fileReading():
     if request.method == 'GET':
         requestFile = request.args.get('filename')
 
         return send_from_directory(app.config['UPLOAD_FOLDER'], requestFile)
-#main function
+
+
+# main function
 if __name__ == '__main__':
     app.run(debug=True)
